0f7788b149a3a495fd2042a501a5b3252aa303fd
AOBDEC <xl.wang@su-go.co.jp>
2025-08-14 07:14:48 +0000

    upd6

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, Response
 import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 @app.route('/SBPaymentResultReceiver/v1/data', methods=['GET', 'POST'])
