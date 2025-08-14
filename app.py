9c83a7887b5bc2eec7fe7766a5c7eb128da46e80
AOBDEC <xl.wang@su-go.co.jp>
2025-08-14 06:21:41 +0000

    upd

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import os
+
 app = Flask(__name__)
 
 @app.route('/pagecon', methods=['GET'])
@@ -7,4 +9,5 @@ def pagecon():
     return "<h2>決済完了</h2>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
